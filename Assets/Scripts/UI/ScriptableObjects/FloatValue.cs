673dd236102546a6057d6d943688ec133c8c7bfb
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2022-03-15 18:08:50 +0100

    Add Float and Int Scriptable Objects

---
+++ Assets/Scripts/UI/ScriptableObjects/FloatValue.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace UI.ScriptableObjects
+{
+    [CreateAssetMenu(menuName = "ScriptableObjects/Values/Float", fileName = "NewFloatValue")]
+    public class FloatValue : ScriptableObject
+    {
+        [SerializeField] private float value;
+
+        public float Value
+        {
+            get => value;
+            set => this.value = value;
+        }
+    }
+}
